b38097d93c62ed871f651eeb23bcf34e75e94fcc
SriramBhagavan <srirambhagavan441@gmail.com>
2025-12-11 17:42:07 +0530

    largest-odd-number-in-string

---
+++ strings/largest_odd_number_in_string.js
@@ -0,0 +1,12 @@
+
+var largestOddNumber = function(num) {
+    let n=num.length
+    while(n>=0){
+        if(Number(num[n]%2===1)){
+            return num.substring(0,n+1)
+        }
+        n--
+    }
+    return ''
+    
+};
